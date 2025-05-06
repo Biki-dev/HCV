5dfe3bda6fa5e846905ec9460f172e8a3ad3d062
Biki Kalita <86558912+Biki-dev@users.noreply.github.com>
2025-05-07 00:04:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,29 +5,38 @@ import Navbar from './components/navbar.jsx';
 import Footer from './components/footer.jsx';
 import Home from './pages/home.jsx';
 import Chapter from './pages/chapter.jsx';
-import Question from './pages/question.jsx'; // This stays
+import Question from './pages/question.jsx';
 import Mock from './pages/mock.jsx';
 import About from './pages/about.jsx';
 import Contect from './pages/contect.jsx';
 import Terms from './pages/terms.jsx';
 
+const mathJaxConfig = {
+  loader: { load: ['[tex]/ams'] },
+  tex: {
+    inlineMath: [['$', '$'], ['\', '\']],
+    displayMath: [['$$', '$$'], ['\', '\']],
+    packages: ['base', 'ams']
+  }
+};
+
 function App() {
   return (
-    <MathJaxContext>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chapter" element={<Chapter />} />
-        <Route path="/question/:chapterId" element={<Question />} />
-        <Route path="/mock" element={<Mock />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/contect" element={<Contect />} />
-      </Routes>
-      <Footer />
-    </Router>
-     </MathJaxContext>
+    <MathJaxContext config={mathJaxConfig}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chapter" element={<Chapter />} />
+          <Route path="/question/:chapterId" element={<Question />} />
+          <Route path="/mock" element={<Mock />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/contect" element={<Contect />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </MathJaxContext>
   );
 }
 
